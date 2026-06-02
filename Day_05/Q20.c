1024b2a84daf09a38cbb93488864d92cda8425ff
Ananya Yadav <ananya03sarita@gmail.com>
2026-06-02 17:41:44 +0530

    Day_05 uploaded

---
+++ Day_05/Q20.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,largestFactor=1;
+    printf("Enter a number:");
+    scanf("%d",&n);
+
+    for(int i=2;i<=n;i++){
+        while(n%i==0){
+            largestFactor=i;
+            n/=i;
+        }
+    }
+    printf("Largest Prime Factor=%d",largestFactor);
+    return 0;
+}
