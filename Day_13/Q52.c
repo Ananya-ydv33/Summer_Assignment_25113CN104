db4a1eff549aab622d11b1cd400f3db942eebae4
Ananya Yadav <ananya03sarita@gmail.com>
2026-06-18 18:46:02 +0530

    Day_13 uploaded

---
+++ Day_13/Q52.c
@@ -0,0 +1,28 @@
+/*write a program to count even and odd elements*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d\n", odd);
+
+    return 0;
+}
