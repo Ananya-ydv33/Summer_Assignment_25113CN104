c0777fb28d212f7c6d8c9c3f583460556f1888e4
Ananya Yadav <ananya03sarita@gmail.com>
2026-06-19 14:00:13 +0530

    Day_21 uploaded

---
+++ Day_21/Q81.c
@@ -0,0 +1,20 @@
+/*write a program to find string length without strlrn().*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    gets(str);   
+
+    // Calculate length manually
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
